5e3b8a5cd146ddb826c1869bb5abcccef36a4972
Jeff Sward <jas983@cornell.edu>
2019-08-08 16:51:47 -0400

    Added a script documenting how to install conda and setup the met4ene environment.

---
+++ config/condainstall.sh
@@ -0,0 +1,19 @@
+# Install Anaconda
+wget "https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh"
+chmod +x Anaconda3-2019.07-Linux-x86_64.sh
+./Anaconda3-2019.07-Linux-x86_64.sh
+yes
+yes
+source ~/.bashrc 
+
+# Initialize anaconda
+conda init tcsh
+source ~/.tcshrc 
+
+# Create conda environment 
+conda create --name met4ene python=2.7
+y
+
+# Install necessary packages
+pip install PyYAML
+pip install requests
