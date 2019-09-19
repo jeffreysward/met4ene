99e83f30850e25f56981b0aad148574b5ae20e49
Jeff Sward <jas983@cornell.edu>
2019-09-19 17:21:25 -0400

    Renamed a bunch of stuff

--- runwrf/examples/t_ga.py
+++ runwrf/examples/t_ga.py
@@ -46,8 +46,7 @@ for generation in range(num_generations):
     print("Generation : ", generation)
     # Measuring the fitness of each chromosome in the population.
     fitness = ga.cal_pop_fitness(equation_inputs, new_population)
-    print("Fitness")
-    print(fitness)
+    print('Fitness: {}'.format(fitness))
 
     best_outputs.append(numpy.max(numpy.sum(new_population * equation_inputs, axis=1)))
     # The best result in the current iteration.
@@ -56,19 +55,16 @@ for generation in range(num_generations):
     # Selecting the best parents in the population for mating.
     parents = ga.select_mating_pool(new_population, fitness,
                                     num_parents_mating)
-    print("Parents")
-    print(parents)
+    print("Parents: ".format(parents))
 
     # Generating next generation using crossover.
     offspring_crossover = ga.crossover(parents,
                                        offspring_size=(pop_size[0] - parents.shape[0], num_weights))
-    print("Crossover")
-    print(offspring_crossover)
+    print("Crossover: ".format(offspring_crossover))
 
     # Adding some variations to the offspring using mutation.
     offspring_mutation = ga.mutation(offspring_crossover, num_mutations=2)
-    print("Mutation")
-    print(offspring_mutation)
+    print("Mutation: ".format(offspring_mutation))
 
     # Creating the new population based on the parents and offspring.
     new_population[0:parents.shape[0], :] = parents
