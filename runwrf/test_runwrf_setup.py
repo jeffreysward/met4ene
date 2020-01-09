2727ce32feeaa24c44b3583aaa06bc37813e7abb
Jeff Sward <jas983@cornell.edu>
2020-01-09 14:47:27 -0500

    Made it so that test_wrfdir_setup and test_prepare_namelists automatically pass if I'm not on Magma, Cheyenne, or AWS.

--- runwrf/test_runwrf_setup.py
+++ runwrf/test_runwrf_setup.py
@@ -6,11 +6,13 @@ from runwrf import determine_computer
 param_ids = [10, 1, 1, 2, 2, 3, 2]
 start_date = 'Jan 15 2011'
 end_date = 'Jan 16 2011'
+on_aws, on_cheyenne, on_magma = determine_computer()
 
-
-def test_determine_computer():
-    on_aws, on_cheyenne, on_magma = determine_computer()
-    print(f'On AWS???: {on_aws}\nOn Cheyenne???: {on_cheyenne}\nOn Magma???: {on_magma}')
+# I can't really think of a good way to test determine_computer without having to call the function over and over again
+# in subsequesnt tests, so I'm just going to omit it for now.
+# def test_determine_computer():
+#     on_aws, on_cheyenne, on_magma = determine_computer()
+    # print(f'On AWS???: {on_aws}\nOn Cheyenne???: {on_cheyenne}\nOn Magma???: {on_magma}')
 
 
 def test_WRFModel():
@@ -22,20 +24,30 @@ def test_WRFModel():
 
 
 def test_get_bc_data():
-    wrf_sim = WRFModel(param_ids, start_date, end_date, setup_yaml='mac_dirpath.yml')
+    wrf_sim = WRFModel(param_ids, start_date, end_date, setup_yaml='linux_dirpath.yml')
     # wrf_sim = WRFModel(param_ids, start_date, end_date)
     vtable_sfx = wrf_sim.get_bc_data()
     assert vtable_sfx == 'ERA-interim.pl'
 
-# def test_wrfdir_setup():
-#     wrf_sim = WRFModel(param_ids, start_date, end_date)
-#     vtable_sfx = wrf_sim.get_bc_data()
-#     wrf_sim.wrfdir_setup(vtable_sfx)
-#     assert 0 == 0
-#
-# def test_prepare_namelists():
-#     wrf_sim = WRFModel(param_ids, start_date, end_date)
-#     vtable_sfx = wrf_sim.get_bc_data()
-#     wrf_sim.wrfdir_setup(vtable_sfx)
-#     wrf_sim.prepare_namelists()
-#     assert 0 == 0
+
+# The next set of tests will only be useful if you're on a machine with the WRF source code downloaded (i.e., Magma,
+# Cheyenne, or AWS). Otherwise, the test will automatically pass. 
+def test_wrfdir_setup():
+    if not on_aws or not on_cheyenne or not on_magma:
+        print('\n!!!Not running test_wrfdir_setup -- switch to Magma, Cheyenne, or AWS.')
+        return
+    wrf_sim = WRFModel(param_ids, start_date, end_date)
+    vtable_sfx = wrf_sim.get_bc_data()
+    wrf_sim.wrfdir_setup(vtable_sfx)
+    assert 0 == 0
+
+
+def test_prepare_namelists():
+    if not on_aws or not on_cheyenne or not on_magma:
+        print('\n!!!Not running test_prepare_namelists -- switch to Magma, Cheyenne, or AWS.')
+        return
+    wrf_sim = WRFModel(param_ids, start_date, end_date)
+    vtable_sfx = wrf_sim.get_bc_data()
+    wrf_sim.wrfdir_setup(vtable_sfx)
+    wrf_sim.prepare_namelists()
+    assert 0 == 0
