38a6f1c1e9a73e1ee36db87979082b4b10206f40
Jeff Sward <jas983@cornell.edu>
2020-03-31 14:03:16 -0400

    Cleaned up and updated a bunch of config files

--- config/create_runwrf_env.sh
+++ config/create_runwrf_env.sh
@@ -1,13 +1,8 @@
 #!/bin/tcsh
-# Install Anaconda
-wget "https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh"
-chmod +x Anaconda3-2019.07-Linux-x86_64.sh
-./Anaconda3-2019.07-Linux-x86_64.sh
-source ~/.bashrc 
+# This script assumes that you already have Anaconda installed and in your path.
 
-# Initialize anaconda
-conda init tcsh
-source ~/.tcshrc 
+# Remove any potential existing directory
+conda env remove --name runwrf
 
 # Create runwrf conda environment
 conda create --name runwrf python=3.7
@@ -19,7 +14,7 @@ pip install requests
 pip install jupyter
 conda install -c conda-forge xarray cartopy wrf-python pynio pseudonetcdf
 pip install siphon
-conda install -c conda-forge nco  # I did this for the magma2 environment only.
+conda install -c conda-forge nco
 conda install -c conda-forge ncl
 echo "Install pvlib-python by cloning the repository and running (pip install -e .) in the directory with setup.py"
 echo "Install optwrf by cloning the met4ene repository and running (pip install -e .) in the met4ene/optwrf/ directory"
