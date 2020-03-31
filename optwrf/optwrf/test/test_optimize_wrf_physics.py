3cfb4f3565d7d4de1fb8a22ca02ae929cb26ccd0
Jeff Sward <jas983@cornell.edu>
2020-03-31 11:04:58 -0400

    Fixed test_optimize_wrf_physics::test_update_sim

--- optwrf/optwrf/test/test_optimize_wrf_physics.py
+++ optwrf/optwrf/test/test_optimize_wrf_physics.py
@@ -8,7 +8,6 @@ from optwrf.optimize_wrf_physics \
     import get_wrf_fitness, run_simplega, conn_to_db, print_database, sql_to_csv, close_conn_to_db
 from optwrf.runwrf import determine_computer
 import optwrf.wrfparams as wp
-import optwrf.runwrf as rw
 import optwrf.simplega as sga
 import optwrf.optimize_wrf_physics as owp
 
@@ -46,18 +45,18 @@ def test_print_database():
 
 
 def test_update_sim():
-    # Generate one individual
-    # dat = rw.format_date(start_date)
-    # param_ids = wp.flexible_generate()
-    individual = sga.Chromosome(param_ids, fitness=100, start_date=start_date)
+    # Generate a random set of parameters, a random start date, and a Chromosome
+    r_start_date, r_end_date = sga.generate_random_dates()
+    r_param_ids = wp.flexible_generate()
+    individual = sga.Chromosome(r_param_ids, fitness=100, start_date=r_start_date, end_date=r_end_date)
     # Put individual in the database
     db_conn = conn_to_db('optwrf_repeat.db')
     owp.insert_sim(individual, db_conn)
     print_database(db_conn)
-    # Generate the same individual again
-    # new_params = wp.flexible_generate()
-    individual = sga.Chromosome(param_ids, fitness=50, start_date='Jan 1 2011')
-    # Update the database
+    # Generate a new random start date and a Chromosome
+    r_start_date, r_end_date = sga.generate_random_dates()
+    individual = sga.Chromosome(r_param_ids, fitness=50, start_date=r_start_date, end_date=r_end_date)
+    # Update the individual in the database database
     owp.update_sim(individual, db_conn)
     print_database(db_conn)
 
