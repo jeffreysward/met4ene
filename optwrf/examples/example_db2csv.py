a172c9b9af4726f8533422e6ec59db7bc8b1319d
Jeff Sward <jas983@cornell.edu>
2020-04-16 22:03:13 -0400

    Added some csvs to enable a restart run.

--- optwrf/examples/example_db2csv.py
+++ optwrf/examples/example_db2csv.py
@@ -11,9 +11,9 @@ import optwrf.optimize_wrf_physics as owp
 
 # Name of the csv file and optwrf database
 #csv_outfile = 'optwrf_database.csv'
-csv_outfile = 'optwrf_database_n10_g2.csv'
+csv_outfile = 'optwrf_database_041620_11am.csv'
 #sql_database = 'optwrf.db'
-sql_database = 'optwrf_n10_g2.db'
+sql_database = 'optwrf_041620_11am.db'
 
 # Connect to the sql database
 db_conn = owp.conn_to_db(sql_database)
