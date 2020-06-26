321f94473a862d3fc2af1aa96ede33c54f957af8
Jeff Sward <jas983@cornell.edu>
2020-06-26 16:52:30 -0400

    Updated the GHI and WPD plotting scripts.

--- optwrf/examples/example_optimize_wrf_physics.py
+++ optwrf/examples/example_optimize_wrf_physics.py
@@ -3,7 +3,7 @@ Optimization of WRF Model Physics
 =================================
 
 If you haven't inhereted a set of WRF model namelists, it's not readily apparent which model setup
-among the ~1 million choices may work the best for your application. This example
+among the ~1.5 million choices may work the best for your application. This example
 runs a simple genetic algorithm to select a set of WRF physics options that preforms the best for
 your application using ERA5 data to benchmark the 12-km WRF simulations.
 """
