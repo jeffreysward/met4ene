11b667b151d5dc96f9fd7f7ce11712304602beeb
Jeff Sward <jas983@cornell.edu>
2020-08-06 15:24:02 -0400

    Added an example to calculate fitness for an arbitrary physics option set and date

---
+++ optwrf/examples/example_get_wrf_fitness.py
@@ -0,0 +1,23 @@
+"""
+Get WRF Fitness
+===============
+
+This example lets you manually determine the fitness for any physics option set on any date
+(provided that it's within the ERA5 date range).
+"""
+
+import optwrf.optimize_wrf_physics as owp
+
+
+# Specify desired physics options set below
+param_ids = [8, 7, 3, 1, 1, 10, 1]
+
+# Specify the desired start date below
+start_date = 'Dec 11  2011'
+end_date = 'Dec 12 2011'
+
+# Run the
+fitness, ghi_mean_error, wpd_mean_error, runtime = owp.get_wrf_fitness(param_ids, start_date, end_date, verbose=True)
+print(f'==========================================================================')
+print(f'Physics Options Set: {param_ids}\tStart Date: {start_date}')
+print(f'Fitness: {fitness}\tGHI Error: {ghi_mean_error}\tWPD Error: {wpd_mean_error}\tSim Runtime: {runtime}')
