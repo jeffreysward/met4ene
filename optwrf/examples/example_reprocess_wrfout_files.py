fb20ce7456d2a71c224d8cce9c582a9221f4cba5
Jeff Sward <jas983@cornell.edu>
2021-03-24 10:56:10 -0400

    Debugging reprocessing script

--- optwrf/examples/example_reprocess_wrfout_files.py
+++ optwrf/examples/example_reprocess_wrfout_files.py
@@ -20,13 +20,11 @@ rootdir = '/share/mzhang/jas983/wrf_data/met4ene/wrfout/ARW/optwrf_output/ow_exp
 #             pass
 
 wrfdir = rootdir + '2011-01-01_19mp4lw1sw7lsm8pbl1cu/'
-if wrfdir.is_dir():
-    print(wrfdir.path)
-    try:
-        # Run the process_wrfout_manual() function in each directory
-        postwrf.process_wrfout_manual(wrfdir.path, 'wrfout_d01.nc', start=None, end=None, save_file=True)
-        # Delete the old wrfout file
-        CMD_RM = 'rm %s'
-        os.system(CMD_RM % (wrfdir.path + 'wrfout_d01.nc'))
-    except:
-        pass
+try:
+    # Run the process_wrfout_manual() function in each directory
+    postwrf.process_wrfout_manual(wrfdir, 'wrfout_d01.nc', start=None, end=None, save_file=True)
+    # Delete the old wrfout file
+    CMD_RM = 'rm %s'
+    # os.system(CMD_RM % (wrfdir + 'wrfout_d01.nc'))
+except:
+    pass
