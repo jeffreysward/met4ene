95855516b520b00a73df60e55b9000df98d6920f
Jeff <jas983@cornell.edu>
2021-09-14 16:00:06 -0400

    Added a new KD Tree class for nearest neighbor lookups

--- optwrf/optwrf/util.py
+++ optwrf/optwrf/util.py
@@ -230,3 +230,89 @@ class Kdtree_ll_to_xy(object):
         dist_sq_min, minindex_1d = self.kdt.query([clat0 * clon0, clat0 * slon0, slat0])
         iy_min, ix_min = np.unravel_index(minindex_1d, self.shape)
         return iy_min, ix_min
+
+
+class KDTreeIndex():
+    
+    """ A KD-tree implementation for fast point lookup on a 2D grid
+    
+    Keyword arguments: 
+    dataset -- a xarray DataArray containing lat/lon coordinates
+               (you can tell this class what their names are with
+               'latvarname' and 'lonvarname')
+               
+    """
+
+    def __init__(self, dataset, latvarname, lonvarname):
+        # store original dataset shape
+        self.shape = dataset.shape
+        
+        # reshape and stack coordinates
+        coords = np.column_stack((dataset[latvarname].values.ravel(),
+                                  dataset[lonvarname].values.ravel()))
+        
+        # construct KD-tree
+        self.tree = cKDTree(self.transform_coordinates(coords))
+    
+    def transform_coordinates(self, coords):
+        """ Transform coordinates from geodetic to cartesian
+        
+        Keyword arguments:
+        coords - a set of lan/lon coordinates (e.g. a tuple or 
+                 an array of tuples)
+        """
+        # WGS 84 reference coordinate system parameters
+        A = 6378.137 # major axis [km]   
+        E2 = 6.69437999014e-3 # eccentricity squared    
+        
+        coords = np.asarray(coords).astype(np.float)
+        
+        # is coords a tuple? Convert it to an one-element array of tuples
+        if coords.ndim == 1:
+            coords = np.array([coords])
+        
+        # convert to radiants
+        lat_rad = np.radians(coords[:,0])
+        lon_rad = np.radians(coords[:,1]) 
+        
+        # convert to cartesian coordinates
+        r_n = A / (np.sqrt(1 - E2 * (np.sin(lat_rad) ** 2)))
+        x = r_n * np.cos(lat_rad) * np.cos(lon_rad)
+        y = r_n * np.cos(lat_rad) * np.sin(lon_rad)
+        z = r_n * (1 - E2) * np.sin(lat_rad)
+        
+        return np.column_stack((x, y, z))
+        
+    def query(self, point):
+        """ Query the kd-tree for nearest neighbour.
+
+        Keyword arguments:
+        point -- a (lat, lon) tuple or array of tuples
+        """
+        _, index = self.tree.query(self.transform_coordinates(point))
+        
+        # regrid to 2D grid
+        index = np.unravel_index(index, self.shape)
+        
+        # return DataArray indexers
+        return xr.DataArray(index[0], dims='pixel'), \
+               xr.DataArray(index[1], dims='pixel')
+    
+    def query_ball_point(self, point, radius):
+        """ Query the kd-tree for all point within distance 
+        radius of point(s) x
+        
+        Keyword arguments:
+        point -- a (lat, lon) tuple or array of tuples
+        radius -- the search radius (km)
+        """
+        
+        index = self.tree.query_ball_point(self.transform_coordinates(point),
+                                           radius)
+
+        # regrid to 2D grid 
+        index = np.unravel_index(index[0], self.shape)
+        
+        # return DataArray indexers
+        return xr.DataArray(index[0], dims='pixel'), \
+               xr.DataArray(index[1], dims='pixel')
