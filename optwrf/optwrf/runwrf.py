869b90ac1173a5465881b2677b0d16dadabc9aa8
Jeff Sward <jas983@cornell.edu>
2022-01-31 08:21:25 -0500

    Changed default to save wps files automaticlly; changed namelist.input template back to 36 levels with the first eta level=1.0

--- optwrf/optwrf/runwrf.py
+++ optwrf/optwrf/runwrf.py
@@ -807,7 +807,7 @@ class WRFModel:
         os.system(self.CMD_RM % (self.DIR_WRFOUT + 'rsl.*'))
         return True
 
-    def run_wrf(self, disable_timeout=False, timeout_hours=8, save_wps_files=False):
+    def run_wrf(self, disable_timeout=False, timeout_hours=8, save_wps_files=True):
         """
         Runs wrf.exe and checks to see if it was successful.
 
