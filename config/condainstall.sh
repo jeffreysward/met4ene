9cd5aeb5ed9c1ce543be15e05c159e9212f8932e
Jeff <jas983@cornell.edu>
2022-03-04 09:48:16 -0500

    Updated how conda env is created

--- config/condainstall.sh
+++ config/condainstall.sh
@@ -3,23 +3,16 @@
 wget "https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh"
 chmod +x Anaconda3-2019.07-Linux-x86_64.sh
 ./Anaconda3-2019.07-Linux-x86_64.sh
-source ~/.bashrc 
+# source ~/.bashrc 
 
 # Initialize anaconda
 conda init tcsh
 source ~/.tcshrc 
 
 # Create runwrf conda environment
-conda create --name runwrf python=3.7
+conda env create -f magma_optwrf_env.yml
 
-# Install necessary packages
-pip install -U pytest
-pip install PyYAML
-pip install requests
-pip install jupyter
-conda install -c conda-forge xarray cartopy wrf-python pynio pseudonetcdf
-pip install siphon
-conda install -c conda-forge nco  # I did this for the magma2 environment only.
-conda install -c conda-forge ncl
+# Remind myself to install 
 echo "Install pvlib-python by cloning the repository and running (pip install -e .) in the directory with setup.py"
 echo "Install optwrf by cloning the met4ene repository and running (pip install -e .) in the met4ene/optwrf/ directory"
+echo "Navigate to ~/conda/envs/optwrf/bin and run 'rm mpi*' (if you fail to do this, WRF will not work properly as it was compiled with executables on the HPC system rather than the one's installed by conda.)"
